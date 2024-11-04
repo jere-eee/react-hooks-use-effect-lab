c9009cb1cc2eb6f578b50a9668cedb4cd91a9048
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-04 12:07:29 +0300

    passing

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setTimeRemaining((prev) => prev - 1)
+    }, 1000)
 
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [timeRemaining])
+
+  if (timeRemaining == 0) {
+    setTimeRemaining(10);
+    onAnswered(false)
+  }
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
